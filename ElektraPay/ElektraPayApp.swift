3d5a61f45ed5c465a4705b58b1ad35604cd46b63
Brayan Gutierrez Juarez <brayangutierrezj0410@gmail.com>
2025-05-15 11:45:53 -0600

    Initial Commit

---
+++ ElektraPay/ElektraPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElektraPayApp.swift
+//  ElektraPay
+//
+//  Created by Brayan Gutierrez Juarez on 15/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ElektraPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ElektraPayTests/ElektraPayTests.swift
@@ -0,0 +1,16 @@
+//
+//  ElektraPayTests.swift
+//  ElektraPayTests
+//
+//  Created by Brayan Gutierrez Juarez on 15/05/25.
+//
+
+import Testing
+
+struct ElektraPayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
