756deae622bf5762d808ff6bd40b2bd6bbb13948
Brayan Gutierrez Juarez <brayangutierrezj0410@gmail.com>
2025-05-15 16:13:53 -0600

    add image

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -14,26 +14,36 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             VStack(spacing: 20) {
+                Text("Elektra")
+                    .font(.system(size: 80, weight: .bold))
+                    .foregroundColor(.red)
+
                 if let product = viewModel.selectedProduct {
                     ProductCard(product: product)
                 } else {
-                    Text("Selecciona un producto")
-                        .foregroundColor(.gray)
+                    Text("Compra facil y rapido en la app de Elektra")
+                        .foregroundColor(.black)
+                        .fontWeight(.bold)
                 }
 
                 Button("Ver productos") {
                     viewModel.loadProducts()
                     showProductList = true
                 }
-                .buttonStyle(.borderedProminent)
+                .font(.title2) // Texto más grande
+                .padding(.vertical, 16)
+                .padding(.horizontal, 40)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(12)
 
                 NavigationLink(destination: ProductListView(viewModel: viewModel),
                                isActive: $showProductList) {
                     EmptyView()
                 }
             }
+            .padding(.bottom, 500)
             .padding()
-            .navigationTitle("Elektra")
         }
     }
 }
